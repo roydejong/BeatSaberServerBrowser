9fb48404b334805799063bba699c5128b99123ba
Roy de Jong <email@roydejong.net>
2020-11-12 15:32:58 +0100

    Cleanup Beat Saver image downloading

---
+++ Utils/CoverArtGrabber.cs
@@ -0,0 +1,36 @@
+﻿using ServerBrowser.Assets;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ServerBrowser.Utils
+{
+    public static class CoverArtGrabber
+    {
+        public static async Task<Sprite> GetCoverArtSprite(string levelId, CancellationToken token)
+        {
+            var level = SongCore.Loader.GetLevelById(levelId);
+
+            if (level != null)
+            {
+                // Official level, or installed custom level found
+                var sprite = await level.GetCoverImageAsync(token);
+                sprite.name = levelId;
+                return sprite;
+            }
+
+            // Level not found locally; ask Beat Saver for cover art
+            var downloadedCover = await BeatSaverHelper.FetchCoverArtBytes(levelId, token);
+
+            if (downloadedCover != null)
+            {
+                var sprite = Sprites.LoadSpriteRaw(downloadedCover);
+                sprite.name = levelId;
+                return sprite;
+            }
+
+            // Failed to get level info, can't set cover art, too bad, very sad
+            return null;
+        }
+    }
+}
