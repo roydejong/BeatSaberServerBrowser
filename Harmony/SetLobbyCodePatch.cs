a2ac8f29ebc5f2894dcd64136f2dfd67eec70cc0
Roy de Jong <email@roydejong.net>
2021-01-21 18:14:11 +0100

    fix(beatsaber): Compatibility with 1.13.2 [#25]
    Updates the lobby code patch to get data from the updated UI (MultiplayerSettingsPanelController rather than HostLobbySetupViewController).
    
    Also bumps manifest to indicate 1.13.2 compatibility.

--- Harmony/SetLobbyCodePatch.cs
+++ Harmony/SetLobbyCodePatch.cs
@@ -6,7 +6,7 @@ namespace ServerBrowser.Harmony
     /// <summary>
     /// This patch lets us retrieve the Server Code for the current lobby.
     /// </summary>
-    [HarmonyPatch(typeof(HostLobbySetupViewController), "SetLobbyCode", MethodType.Normal)]
+    [HarmonyPatch(typeof(MultiplayerSettingsPanelController), "SetLobbyCode", MethodType.Normal)]
     public static class SetLobbyCodePatch
     {
         public static void Postfix(string code)
