c5ff434950dd0417e331ea2758ff5a8dc6b866fb
Roy de Jong <roy@softwarepunt.nl>
2022-01-12 16:18:20 +0100

    wip(rewrite): new ui - level bar component, color test

--- UI/Components/LevelBarClone.cs
+++ UI/Components/LevelBarClone.cs
@@ -38,22 +38,10 @@ namespace ServerBrowser.UI.Components
         #endregion
 
         #region UI Access
-        public ImageView Image { get; private set; } = null!;
-
+        private ImageView _bg = null!;
+        private ImageView _image = null!;
         private CurvedTextMeshPro _titleText = null!;
         private CurvedTextMeshPro _secondaryText = null!;
-
-        public string TitleText
-        {
-            get => _titleText.text;
-            set => _titleText.text = value;
-        }
-
-        public string SecondaryText
-        {
-            get => _secondaryText.text;
-            set => _secondaryText.text = value;
-        }
         
         private void InitUI()
         {
@@ -62,15 +50,57 @@ namespace ServerBrowser.UI.Components
                 Destroy(hoverHint);
             
             // Parts
-            Image = transform.Find("SongArtwork").GetComponent<ImageView>();
-            
             var singleLineTextContainer = transform.Find("SingleLineTextContainer");
             singleLineTextContainer.gameObject.SetActive(true);
+            transform.Find("MultipleLineTextContainer").gameObject.SetActive(false);
             
+            _bg = transform.Find("BG").GetComponent<ImageView>();
+            _image = transform.Find("SongArtwork").GetComponent<ImageView>();
             _titleText = singleLineTextContainer.Find("SongNameText").GetComponent<CurvedTextMeshPro>();
             _secondaryText = singleLineTextContainer.Find("AuthorNameText").GetComponent<CurvedTextMeshPro>();
-            
-            transform.Find("MultipleLineTextContainer").gameObject.SetActive(false);
+        }
+
+        public enum BackgroundStyle : byte
+        {
+            GameDefault,
+            Test,
+            GrayTitle
+        }
+        
+        public void SetBackgroundStyle(BackgroundStyle style = BackgroundStyle.GameDefault)
+        {
+            // Primary background color
+            _bg.color = style switch
+            {
+                BackgroundStyle.Test => Color.white,
+                BackgroundStyle.GrayTitle => new Color(1, 1, 1, .2f),
+                _ => Color.black
+            };
+            // Gradient left color
+            _bg.color0 = style switch
+            {
+                BackgroundStyle.Test => new Color(0, .55f, .99f, 0f),
+                BackgroundStyle.GrayTitle => Color.white,
+                _ => new Color(1, 1, 1, 0)
+            };
+            // Gradient right color
+            _bg.color1 = style switch
+            {
+                BackgroundStyle.Test => new Color(1f, 0, .5f, 1f),
+                BackgroundStyle.GrayTitle => new Color(1, 1, 1, 0),
+                _ => new Color(1, 1, 1, .3f)
+            };
+        }
+
+        public void SetImageSprite(Sprite? sprite)
+        {
+            _image.sprite = sprite;
+        }
+        
+        public void SetText(string titleText, string secondaryText)
+        {
+            _titleText.SetText(titleText);
+            _secondaryText.SetText(secondaryText);
         }
         #endregion
     }
--- UI/Views/ServerBrowserDetailViewController.cs
+++ UI/Views/ServerBrowserDetailViewController.cs
@@ -1,5 +1,8 @@
 using HMUI;
+using ServerBrowser.Assets;
 using ServerBrowser.UI.Components;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace ServerBrowser.UI.Views
 {
@@ -7,9 +10,19 @@ namespace ServerBrowser.UI.Views
     {
         public void Awake()
         {
-            var ldc = LevelBarClone.Create(transform);
-            ldc.TitleText = "Game server name, which may be long";
-            ldc.SecondaryText = "BeatTogether Dedicated Server";
+            var rootContainer = new GameObject("testRoot");
+            rootContainer.transform.SetParent(transform);
+            rootContainer.AddComponent<VerticalLayoutGroup>();
+            
+            var ldc = LevelBarClone.Create(rootContainer.transform);
+            ldc.SetBackgroundStyle(LevelBarClone.BackgroundStyle.Test);
+            ldc.SetImageSprite(Sprites.Pencil);
+            ldc.SetText("Primary", "Secondary");
+            
+            var ldc2 = LevelBarClone.Create(rootContainer.transform);
+            ldc2.SetBackgroundStyle(LevelBarClone.BackgroundStyle.GrayTitle);
+            ldc2.SetImageSprite(Sprites.Pencil);
+            ldc2.SetText("Primary2", "Secondary2");
         }
 
         public void SetData(object serverInfo)
