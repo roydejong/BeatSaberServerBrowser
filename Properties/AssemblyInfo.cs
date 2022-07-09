ac46bfd1339ca2b4558aa1c1cd911c6d2825c9be
Roy de Jong <roy@softwarepunt.nl>
2022-07-09 17:28:15 +0200

    maint: bump version to 1.1.0

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -12,5 +12,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("7e8f3dda-fe61-4a71-9974-560a6bc1c286")]
 
 [assembly: AssemblyCulture("en-US")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
