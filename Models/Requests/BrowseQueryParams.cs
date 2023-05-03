957161b4fac913ba4052c7ebacb592860f8553fd
Roy de Jong <roy@softwarepunt.nl>
2023-05-03 23:50:42 +0200

    wip(unity-beta): move to internal querystring helper

--- Models/Requests/BrowseQueryParams.cs
+++ Models/Requests/BrowseQueryParams.cs
@@ -1,5 +1,4 @@
-using System;
-using System.Web;
+using ServerBrowser.Utils;
 
 namespace ServerBrowser.Models.Requests
 {
@@ -17,7 +16,7 @@ namespace ServerBrowser.Models.Requests
         /// Indicates whether any filters have been activated.
         /// </summary>
         public bool AnyFiltersActive =>
-            !String.IsNullOrEmpty(TextSearch) || HideFullGames || HideModdedGames || HideVanillaGames
+            !string.IsNullOrEmpty(TextSearch) || HideFullGames || HideModdedGames || HideVanillaGames
             || HideInProgressGames || HideQuickPlay;
 
         public void Reset()
@@ -32,30 +31,30 @@ namespace ServerBrowser.Models.Requests
 
         public string ToQueryString()
         {
-            var queryString = HttpUtility.ParseQueryString("");
+            var queryString = new QueryString();
 
-            queryString.Add("limit", 7.ToString());
-            queryString.Add("includeLevel", "1");
+            queryString.Set("limit", 7.ToString());
+            queryString.Set("includeLevel", "1");
             
             if (Offset > 0)
-                queryString.Add("offset", Offset.ToString());
+                queryString.Set("offset", Offset.ToString());
             
-            if (!String.IsNullOrEmpty(TextSearch))
-                queryString.Add("query", TextSearch);
+            if (!string.IsNullOrEmpty(TextSearch))
+                queryString.Set("query", TextSearch);
             
             if (HideFullGames)
-                queryString.Add("filterFull", "1");
+                queryString.Set("filterFull", "1");
             
             if (HideModdedGames)
-                queryString.Add("filterModded", "1");
+                queryString.Set("filterModded", "1");
             else if (HideVanillaGames)
-                queryString.Add("filterVanilla", "1");
+                queryString.Set("filterVanilla", "1");
             
             if (HideInProgressGames)
-                queryString.Add("filterInProgress", "1");
+                queryString.Set("filterInProgress", "1");
             
             if (HideQuickPlay)
-                queryString.Add("filterQuickPlay", "1");
+                queryString.Set("filterQuickPlay", "1");
 
             return queryString.ToString();
         }
